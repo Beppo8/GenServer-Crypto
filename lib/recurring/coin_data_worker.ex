c568e043ea079a348eb2c42330b7c0f635ab514c
beppo <beppo@localhost.localdomain>
2021-01-07 22:03:35 -0600

    coin_data & refactor

--- lib/recurring/coin_data_worker.ex
+++ lib/recurring/coin_data_worker.ex
@@ -1,8 +1,11 @@
 defmodule Teacher.CoinDataWorker do
   use GenServer
 
+  alias Teacher.CoinData
+
   def start_link(args) do
-    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+    id = Map.get(args, :id)
+    GenServer.start_link(__MODULE__, args, name: id)
   end
 
   def init(state) do
@@ -11,19 +14,36 @@ defmodule Teacher.CoinDataWorker do
   end
 
   def handle_info(:coin_fetch, state) do
-    price = coin_price()
-    IO.inspect("Current Bitcoin price $#{price}")
+    updated_state = state
+      |> Map.get(:id)
+      |> CoinData.fetch()
+      |> update_state(state)
+
+    if updated_state[:price] != state[:price] do
+      IO.inspect("Current #{updated_state[:name]} price $#{updated_state[:price]}")
+    end
+
     schedule_coin_fetch()
-    {:noreply, Map.put(state, :btc, price)}
+    {:noreply, updated_state}
   end
 
-  defp coin_price do
-    "http://coincap.io/page/BTC"
-    |> HTTPoison.get!()
-    |> Map.get(:body)
-    |> Jason.decode!()
-    |> Map.get("price_usd")
+  defp update_state(%{"display_name" => name, "price_usd" => price}, existing_state) do
+    Map.merge(existing_state, %{name: name, price: price})
   end
+
+  # defp coin_data(id) do
+  #   id
+  #   |> Atom.to_string()
+  #   |> String.upcase()
+  #   |> url()
+  #   |> HTTPoison.get!()
+  #   |> Map.get(:body)
+  #   |> Jason.decode!()
+  # end
+
+  # defp url(id) do
+  #   "http://coincap.io/page/BTC" <> id
+  # end
 
   defp schedule_coin_fetch do
     Process.send_after(self(), :coin_fetch, 5_000)
